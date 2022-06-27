0009c628b224b7104a6071a0dcbfbdb17d058932
masfranzhuo <masfranzhuo@gmail.com>
2022-06-27 14:04:39 +0700

    add localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
+import 'package:flutter_i18n/loaders/decoders/yaml_decode_strategy.dart';
+import 'package:flutter_i18n/loaders/namespace_file_translation_loader.dart';
 import 'package:flutter_project/core/di/injector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_project/src/presentation/pages/main_page.dart';
 
 Future<void> main() async {
-  /// TODO: multi language
   /// TODO: environment
   WidgetsFlutterBinding.ensureInitialized();
+  final FlutterI18nDelegate flutterI18nDelegate = FlutterI18nDelegate(
+    translationLoader: NamespaceFileTranslationLoader(
+      namespaces: ['label'],
+      useCountryCode: true,
+      fallbackDir: 'en_US',
+      basePath: 'assets/flutter_i18n',
+      // forcedLocale: const Locale('id', 'ID'),
+      decodeStrategies: [YamlDecodeStrategy()],
+    ),
+    missingTranslationHandler: (key, locale) {
+      if (kDebugMode) {
+        print("--- Missing Key: $key, languageCode: ${locale?.languageCode}");
+      }
+    },
+  );
   await dotenv.load();
   configureDependencies();
-  runApp(const MainPage());
+  runApp(MainPage(flutterI18nDelegate: flutterI18nDelegate));
 }
--- lib/src/presentation/pages/main_page.dart
+++ lib/src/presentation/pages/main_page.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
 import 'package:flutter_project/src/presentation/pages/home_page/home_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
+  final FlutterI18nDelegate flutterI18nDelegate;
 
-  // This widget is the root of your application.
+  const MainPage({
+    Key? key,
+    required this.flutterI18nDelegate,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      localizationsDelegates: [
+        flutterI18nDelegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('id', 'ID'),
+      ],
+      home: const HomePage(title: 'Home Page'),
     );
   }
 }

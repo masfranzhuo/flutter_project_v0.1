fad4cfedc3d63d987329627684bbd3a208383ebc
Masfran <masfranzhuo@gmail.com>
2022-07-28 00:25:03 +0700

    Development (#6)
    * add localizations
    
    * fix home page title
    
    * add env config
    
    * add user entity and data source test
    
    * build test on dev branch
    
    * create file on test
    
    * add user repository test
    
    * git ignore
    
    * add use case and state manager test for user
    
    * add user presentation
    
    * add doc
    
    * add build_runner build on test workflow
    
    * delete pagination config
    
    * delete mock test file
    
    * remove duplicate import
    
    * add doc
    
    * fix some code
    
    * add responsive
    
    * fix users and user detail page test
    
    * fix base url
    
    * refactor env config
    
    * add doc
    
    * fix some code
    
    * add responsive
    
    * fix users and user detail page test
    
    * fix base url
    
    * refactor env config
    
    * refactor http client service
    
    * fix typo
    
    * minor fix test
    
    * add hive as local storage service
    
    * add sqflite
    
    * rename user local data source
    
    * fix flutter version test
    
    * update hive and date format
    
    * update hive
    
    * fix date config
    
    * fix exception
    
    * use device preview and upgrade to latest sdk
    
    * fix test
    
    * fix test
    
    * add doc
    
    * add responsive
    
    * fix users and user detail page test
    
    * add localizations
    
    * fix home page title
    
    * add env config
    
    * add user entity and data source test
    
    * add user repository test
    
    * add use case and state manager test for user
    
    * add user presentation
    
    * add responsive
    
    * fix users and user detail page test
    
    * refactor env config
    
    * refactor http client service
    
    * fix typo
    
    * minor fix test
    
    * add hive as local storage service
    
    * add sqflite
    
    * rename user local data source
    
    * fix flutter version test
    
    * update hive and date format
    
    * update hive
    
    * fix date config
    
    * fix exception
    
    * use device preview and upgrade to latest sdk
    
    * fix test
    
    * fix test
    
    * remove use case util and rename pages args
    
    * remove fvm and vscode config

--- lib/src/use_cases/get_user.dart
+++ lib/src/use_cases/get_user.dart
@@ -1,18 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_project/core/utils/failure.dart';
-import 'package:flutter_project/core/utils/use_case.dart';
 import 'package:flutter_project/src/entities/user.dart';
 import 'package:flutter_project/src/repositories/user_repository.dart';
 import 'package:injectable/injectable.dart';
 
 @lazySingleton
-class GetUser extends UseCase<User, String> {
+class GetUser {
   final UserRepository repository;
 
   GetUser({required this.repository});
 
-  @override
-  Future<Either<Failure, User>> call(String params) async {
-    return repository.getUser(id: params);
+  Future<Either<Failure, User>> call({required String id}) async {
+    return repository.getUser(id: id);
   }
 }

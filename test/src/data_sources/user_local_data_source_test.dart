f15ac913b187545c65acb3c62014a006c7150132
masfranzhuo <masfranzhuo@gmail.com>
2022-08-12 14:25:45 +0700

    fix test

--- test/src/data_sources/user_local_data_source_test.dart
+++ test/src/data_sources/user_local_data_source_test.dart
@@ -43,6 +43,7 @@ void main() {
   late MockUser mockUser;
 
   setUp(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
     await Isar.initializeIsarCore(download: true);
     isar = await Isar.open(
       [LocationIsarSchema, UserIsarSchema],
